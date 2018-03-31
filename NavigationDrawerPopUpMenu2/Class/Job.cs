1d9ea831a171d29d32f5de389b75b6f4f8d3d556
KiritoNguyen <nguyenphucphi12@gmail.com>
2018-03-31 21:53:53 +0700

    Update_TuyenDung

---
+++ NavigationDrawerPopUpMenu2/Class/Job.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationDrawerPopUpMenu2
+{
+    public class Job
+    {
+        private string image;
+        private string title;
+        private string content;
+        private string location;
+        private string money;
+        private string link;
+        public string Image { get => image; set => image = value; }
+        public string Title { get => title; set => title = value; }
+        public string Content { get => content; set => content = value; }
+        public string Location { get => location; set => location = value; }
+        public string Money { get => money; set => money = value; }
+        public string Link { get => link; set => link = value; }
+    }
+}
--- NavigationDrawerPopUpMenu2/MainWindow.xaml.cs
+++ NavigationDrawerPopUpMenu2/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace NavigationDrawerPopUpMenu2
 
     public partial class MainWindow : Window
     {
-        ObservableCollection<object> listcongnghe;
+        ObservableCollection<object> listcongnghe, listtuyendung;
         HttpClient httpclient;
         HttpClientHandler handler;
         CookieContainer cookie;
@@ -22,6 +22,9 @@ namespace NavigationDrawerPopUpMenu2
             InitializeComponent();
 
             listcongnghe = new ObservableCollection<object>();
+
+
+            listtuyendung = new ObservableCollection<object>();
 
             cookie = new CookieContainer();
             handler = new HttpClientHandler()
@@ -33,18 +36,19 @@ namespace NavigationDrawerPopUpMenu2
                 UseDefaultCredentials = false
             };
             httpclient = new HttpClient(handler);
-            web.WebView.NewWindow += (sender,e) =>
+            web.WebView.NewWindow += (sender, e) =>
             {
                 Process.Start(e.TargetUrl);
             };
+
         }
-        
+
         private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
         {
             ButtonCloseMenu.Visibility = Visibility.Visible;
             ButtonOpenMenu.Visibility = Visibility.Collapsed;
         }
-         
+
         private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
         {
             ButtonCloseMenu.Visibility = Visibility.Collapsed;
@@ -56,10 +60,11 @@ namespace NavigationDrawerPopUpMenu2
             switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
             {
                 case "ItemTech":
+
                     listcongnghe.Clear();
                     new Task(() =>
                     {
-                       Crawl_Tech_Data("https://techtalk.vn/tech");
+                        Crawl_Tech_Data("https://techtalk.vn/tech");
                     }).Start();
                     new Task(() =>
                     {
@@ -68,9 +73,15 @@ namespace NavigationDrawerPopUpMenu2
                     new Task(() =>
                     {
                         Crawl_Tech_Data("https://topdev.vn/blog/category/cong-nghe/");
-                    }).Start();    
+                    }).Start();
                     break;
-                case "ItemStudy":
+                case "ItemJob":
+                    listtuyendung.Clear();
+                    new Task(() =>
+                    {
+                        Crawl_Job_Data("https://topdev.vn/it-jobs/");
+                    }).Start();
+
                     break;
                 case "ItemExit":
                     Application.Current.Shutdown();
@@ -86,10 +97,59 @@ namespace NavigationDrawerPopUpMenu2
         {
             Application.Current.Shutdown();
         }
+
+        public void Crawl_Job_Data(string url)
+        {
+            string html = Crawl.Instance.CrawlDataFromUrl(url, httpclient);
+            var Ne = Regex.Matches(html, @"<div class=""clearfix box-top-level""(.*?)</div>(.*?)</div>(.*?)</div>(.*?)</div>(.*?)</div>(.*?)</div>", RegexOptions.Singleline);
+
+            foreach (var item in Ne)
+            {
+                Job jobs = new Job();
+                var LinkAndImage = Regex.Match(item.ToString(), @"<div class=""logo"">(.*?)</div>", RegexOptions.Singleline);
+                // lấy image
+                string image = Regex.Match(LinkAndImage.ToString(), @"src=""(.*?)""", RegexOptions.Singleline).Value.Replace(@"src=""", "").Replace(@"""", "");
+                jobs.Image = image;
+                // lấy link,title
+                var Links = Regex.Match(item.ToString(), @"<div class=""job-item-info relative"">(.*?)</div>", RegexOptions.Singleline);
+
+                string link = Regex.Match(Links.ToString(), @"href=""(.*?)""", RegexOptions.Singleline).Value.Replace(@"href=""", "").Replace(@"""", "");
+                string temp = Regex.Match(Links.ToString(), @"<a (.*?)</a>", RegexOptions.Singleline).Value;
+                string title = temp.Substring(temp.IndexOf('>') + 1).Replace("</a>", "").Trim();
+                jobs.Link = link;
+                jobs.Title = title;
+                //lấy company
+                string company = Regex.Match(Links.ToString(), @"<div class=""company"">(.*?)</div>", RegexOptions.Singleline).Value.Replace(@"<div class=""company"">", "").Replace(@"""", "").Replace("</div>", "");
+                jobs.Content = company;
+                //lấy location
+                string temp_locate = Regex.Match(item.ToString(), @"<div class=""extra-info location text-clip"">(.*?)</div>", RegexOptions.Singleline).Value;
+                string locations = Regex.Match(temp_locate.ToString(), @"<span>(.*?)</span>", RegexOptions.Singleline).Value.Replace(@"<span>", "").Replace(@"""", "").Replace("</span>", "");
+
+                jobs.Location = locations;
+                //lấy salary
+                string info_salary = Regex.Match(item.ToString(), @"<div class=""extra-info salary"">(.*?)</div>", RegexOptions.Singleline).Value;
+                string temp_salary = Regex.Match(info_salary.ToString(), @" <a (.*?)</a>", RegexOptions.Singleline).Value.Replace(@"<a ", "").Replace(@"""", "").Replace("</a>", "");
+                string salary = temp_salary.Substring(temp_salary.IndexOf('>') + 1);
+                jobs.Money = salary;
+                this.Dispatcher.Invoke(() => { ListViewNews.Items.Add(jobs); });
+                ListViewNews.AddHandler(ListViewItem.MouseDoubleClickEvent, new MouseButtonEventHandler(job_MouseDoubleClick));
+            }
+
+        }
+        public void job_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            string link = (ListViewNews.SelectedItem as Job).Link;
+            web.WebView.Url = null;
+            web.WebView.LoadUrl(link);
+        }
+
         public void Crawl_Tech_Data(string url)
         {
             string html = Crawl.Instance.CrawlDataFromUrl(url, httpclient);
             var News = Regex.Matches(html, @"<div class=""(td_module_10|td_module_mx2|td_module_mx8) td_module_wrap td-animation-stack""(.*?)</div>(.*?)</div>(.*?)</div>", RegexOptions.Singleline);
+
+
             foreach (var item in News)
             {
                 New congnghe = new New();
@@ -113,6 +173,7 @@ namespace NavigationDrawerPopUpMenu2
                 string content = Temp_content.Substring(Temp_content.IndexOf('>') + 1).Replace("</div>", "").Trim();
                 congnghe.Content = content;
                 this.Dispatcher.Invoke(() => { ListViewNews.Items.Add(congnghe); });
+
             }
             ListViewNews.AddHandler(ListViewItem.MouseDoubleClickEvent, new MouseButtonEventHandler(item_MouseDoubleClick));
         }
